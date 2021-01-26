9eec4d8bd899606e4c0369e201a133099babd707
Lennart <dross@campus.tu-berlin.de>
2021-01-26 19:16:53 +0100

    offset more precise? v17

--- ntpclient.c
+++ ntpclient.c
@@ -126,19 +126,19 @@ int main(int argc, char **argv) {
         freeaddrinfo(servinfo);
 
         for (int j = 0; j < n; j++) {
+            addr_len = sizeof(their_addr);
             if ((numbytes = sendto(sockfd, msg, 48, 0, p->ai_addr, p->ai_addrlen)) == -1) {
                 perror("ntpclient: sendto()");
                 exit(1);
             }
             clock_gettime(CLOCK_REALTIME, &t[0]); // TODO: check for errors, maybe move behind sendto()?
 
-            fprintf(stderr, "ntpclient: sent %d bytes to %s\n", numbytes, argv[i]);
-            fprintf(stderr, "ntpclient: waiting to recvfrom...\n");
+//            fprintf(stderr, "ntpclient: sent %d bytes to %s\n", numbytes, argv[i]);
+//            fprintf(stderr, "ntpclient: waiting to recvfrom...\n");
 
-            addr_len = sizeof(their_addr);
             numbytes = 0;
             while ( numbytes < 48 ) {
-                memset(buf, 0 , 48);
+//                memset(buf, 0 , 48);
                 if ((numbytes = recvfrom(sockfd, buf, 48, 0, (struct sockaddr *) &their_addr, &addr_len)) == -1) {
                     perror("recvfrom");
                     exit(1);
